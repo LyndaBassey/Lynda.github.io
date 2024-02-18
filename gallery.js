c0f96802f5e101169fc39dd81c773558aff24556
LyndaBassey <107851063+LyndaBassey@users.noreply.github.com>
2024-02-18 01:42:38 +0100

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,123 @@
+// const imagesData = [
+    // {
+    //   img: "./images/WhatsApp Image 2023-07-01 at 6.58.58 PM (1).jpeg",
+    //   name: "Nelly Cheiboi",
+    //   descripiton:
+    //     "Chimamanda ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    // },
+    // {
+    //   img: "./images/WhatsApp Image 2023-07-05 at 8.03.09 AM.jpeg",
+    //   name: "Diara Bousso",
+    //   descripiton:
+    //     "Okafor ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    // },
+    // {
+    //   img: "./images/WhatsApp Image 2023-07-10 at 12.01.42 PM.jpeg",
+    //   name: "Maria Stevens",
+    //   descripiton:
+    //     "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    // },
+    // {
+    //     img: "./images/WhatsApp Image 2023-07-07 at 9.34.11 AM.jpeg",
+    //     name: "Lumbie Mlambo",
+    //     descripiton:
+    //       "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    //   },
+    //   {
+    //     img: "./images/WhatsApp Image 2023-07-09 at 6.49.28 AM.jpeg",
+    //     name: "Alice Mamaga Amoako",
+    //     descripiton:
+    //       "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    //   },
+    //   {
+    //     img: "./images/WhatsApp Image 2023-07-11 at 12.13.47 PM.jpeg",
+    //     name: "Aisha Pandor",
+    //     descripiton:
+    //       "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+    //   },
+    //   {
+  //       img: "./images/WhatsApp Image 2023-07-12 at 10.40.30 AM.jpeg",
+  //       name: "Charlette N'guessan",
+  //       descripiton:
+  //         "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+  //     },
+  //     {
+  //       img: "./images/WhatsApp Image 2023-07-06 at 10.19.26 AM.jpeg",
+  //       name: "Gisela Van Houcke",
+  //       descripiton:
+  //         "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
+  //     },
+  // ];
+  
+  // const modal = document.getElementById("myModal");
+  // const closeBtn = document.getElementsByClassName("close_btn")[0];
+  // const imgContainer = document.querySelector(".gallery");
+  // const prevbtn = document.getElementById("prev");
+  // const nextbtn = document.getElementById("next");
+  // const slideImg = document.getElementById("slider_image");
+  // const modalTitle = document.getElementById("modal_title");
+  // const modalBodyText = document.getElementById("modal_body");
+  
+  // let count = 0;
+  
+  // // close modal if user clicks anywhere out of the modal
+  // window.onclick = function (event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // };
+  
+  // // Display images on page load
+  // imagesData.forEach((data, index) => {
+  //   const imgWrapper = document.createElement("div");
+  //   imgWrapper.classList.add("img_wrapper");
+  
+  //   const imgElement = document.createElement("img");
+  //   imgElement.src = data.img;
+  //   imgElement.alt = data.name;
+  
+  //   imgWrapper.appendChild(imgElement);
+  //   imgContainer.appendChild(imgWrapper);
+  
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
